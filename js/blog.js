8f003d90c14eefb9d8d53fd2f0b51a593099b4ce
冷鱼闲风 <39406598+2991883280@users.noreply.github.com>
2020-10-07 14:35:07 +0800

    5

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,5 @@
-	//获取网址参数
+	//博客数据存储	
+//获取网址参数
 				function GetQueryString(name) {
      var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
      var r = window.location.search.substr(1).match(reg);
@@ -34,33 +35,57 @@ var dimeAxis=[
 */
 
 
-//列表处理，看不懂就不要动！
+//列表处理
 $(function(){
-	$.each(dimeAxis,function(i,e){
-		if(i==0){$('.time-axis').empty();}
+	$.each(dimeAxisl,function(i,e){
+	if(i==0){
+		var ht=' <br><p style="width: 100%;text-align: center;font-size: 50px;height: 40px;color:#000000;font-weight:600;">最新文章</p>';
+			$('.time-').append(ht);
+		}
+		
+		if(i<5){
+			var h=' <div class="single-feature">'+
+			'   <a href="./blogw.html?id='+i+'" class="title" > <img href="img/product.htmlofter.jpg">'+
+			'<h3>《'+e.title1+'》</h3></a>'+
+			'<p>作者：'+e.author1+'，发表日期：'+e.data1+'</p>'+
+			'</div>';
+		$('.time-').append(h);
+			
+			
+		}
+		
+
+	
+	
+	
 		var html=' <div class="single-feature">'+
 			'   <a href="./blogw.html?id='+i+'" class="title" > <img href="img/product.htmlofter.jpg">'+
-			'<h3>《'+e.title+'》</h3></a>'+
-			'<p>作者：'+e.author+'，发表日期：'+e.data+'</p>'+
+			'<h3>《'+e.title1+'》</h3></a>'+
+			'<p>作者：'+e.author1+'，发表日期：'+e.data1+'</p>'+
 			'</div>';
 		$('.time-axis').append(html);
 		
+	
 		if(i==GetQueryString("id")){
 			
 		var html=''+
 			'  <img href="img/product.htmlofter.jpg">'+
 		
-			'<iframe class="   -webkit-user-drag: none;	-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;" height="1600" width="100%" src="'+e.url+'" frameborder="0" allowfullscreen></iframe>';
+			'<iframe class="   -webkit-user-drag: none;	-webkit-border-radius: 10px;-moz-border-radius: 10px;border-radius: 10px;" height="1600" width="100%" src="'+e.url1+'" frameborder="0" allowfullscreen></iframe>';
 			$('.left-part').empty();
 			$('.time-axi').empty();
 		$('.time-axi').append(html);
 		
-		var htm='<h1>《'+e.title+'》</h1><p>作者：'+e.author+'</p><p>日期：'+e.data+'</p>';
+		var htm='<h1>《'+e.title1+'》</h1><p>作者：'+e.author1+'</p><p>日期：'+e.data1+'</p>';
 		$('.left-part').append(htm);
 		}
 		
 		
 		
 	});
+	
+	
+	
+	
 });
  
