1316260fcdcbe3a723257054be8799476ab4a1fc
冷鱼闲风 <39406598+2991883280@users.noreply.github.com>
2020-10-07 14:59:10 +0800

    4

--- js/cp1.js
+++ js/cp1.js
@@ -7,27 +7,28 @@ var dimeAxis=[
 	author:'陈年老按',
 	url:'https://www.lanzoux.com/i63YIh8n5mf'
 },	
-	
 {
 	data:'聚合编程猫社区<20条API，获取/提交数据更简单~',
-	title:'编程猫社区API库-py源',
+	title:'编程猫社区API库-py源文件',
 	author:'冷鱼闲风',
 	url:'https://www.lanzoux.com/iCiVhh7edpg'
 },
+
+
 {
 	data:'自动发送多条一言消息到指定作品评论区！',
-	title:'编程猫社区作品一言评论-py源',
+	title:'编程猫社区作品一言评论-py源文件',
 	author:'冷鱼闲风',
 	url:'https://www.lanzoux.com/ievquh7ebva'
 },
 {
 	data:'一个可以支持批量翻译文本的应用。',
-	title:'有道翻译器-py源',
+	title:'有道翻译器-py源文件',
 	author:'冷鱼闲风',
 	url:'https://www.lanzoux.com/iIFjbh7e15e'
 },{
 	data:'自动回复指定作品下的评论。做到15s内秒回~',
-	title:'AI 编程猫社区作品评论机器人-py源',
+	title:'AI 编程猫社区作品评论机器人-py源文件',
 	author:'冷鱼闲风',
 	url:'https://www.lanzoux.com/iCqCsh7e7yj'
 },
@@ -51,15 +52,7 @@ $(function(){
 		
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		var html=' <div class="single-feature">'+
+var html=' <div class="single-feature">'+
 			'   <a href="'+e.url+'" class="title" target="_blank"> <img href="img/product.htmlofter.jpg">'+
 			'<h3>'+e.title+'</h3></a>'+
 			'<p>'+e.data+'</p>'+
