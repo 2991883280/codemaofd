5e0c9c05d9004f50ac112d6dc18c99c78627b5d2
冷鱼闲风 <39406598+2991883280@users.noreply.github.com>
2021-04-04 21:13:36 +0800

    Update video.js

--- js/video.js
+++ js/video.js
@@ -20,7 +20,8 @@ var dimeAxis=[
 $(function(){
 	$.each(dimeAxis,function(i,e){
 		
-			$('.time-axis').empty();
+			
+		if(i==0){$('.time-axis').empty();}
 var htmll='<div class="video">'+
               '<h1>'+e.title+'</h1>'+
 			  '<hr  size="15px" color="#777777">'+
